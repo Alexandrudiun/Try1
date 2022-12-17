d0c68c847e805d93d1c17a15c37f88d8b78a4e74
Alexandru Dinu <76495571+Alexandrudiun@users.noreply.github.com>
2022-12-17 11:24:50 +0200

    Add files via upload

---
+++ try 1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <csbootcamp.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+//4biti = nibble (halfbyte);
+
+typedef struct Masina
+{
+ char* marca;
+ char *model;
+ char tokenMasina[11];
+ int pretAchizitie;
+ int pretVanzare;
+}Masina;
+
+int main(int argc, char *argv[])
+{
+
+char *input;
+    int v[2700];
+
+FILE *inputCerinte = fopen(argv[1], "r");
+    if(file==NULL)perror("Error opening file");
+    return 1;}
+    FILE *inputDate = fopen(argv[2], "r");
+     if(file==NULL)perror("Error opening file");
+    return 1;}
+   
+
+    int suma=0;
+    while(reading)
+        { suma=0;
+         scanf(input,"%s)
+
+            for(int i =0; i<strlen(input);i++)
+            {
+                suma=suma+int(toupper(char *input[i]));
+            }
+            v[suma]++;
+        }
+        
+}
